ec13864ff73206927b454dcccb6fa27761d114b1
Sagar Vishwakarma <123996058+sagarv8090@users.noreply.github.com>
2025-07-03 21:58:03 +0530

    Time: 0 ms (100%), Space: 8.9 MB (26.42%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.cpp
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -1,21 +1,10 @@
 class Solution {
-
 public:
-
-    static int scoreOfString(string& s) {
-
-        adjacent_difference(s.begin(), s.end(), s.begin());
-
-        
-
-        //sum the absolute values of the differences, starting from the second element
-
-        return accumulate(s.begin()+1, s.end(), 0, [](int x, int y){
-
-            return x+abs(y);
-
-        });
-
+    int scoreOfString(string s) {
+        int sum=0;
+        for(int i=0;i<s.size()-1;i++){
+            sum+=abs(int(int(s[i])-int(s[i+1])));
+        }
+        return sum;
     }
-
 };
